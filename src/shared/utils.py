02626850f04211edd4fe450c0c3cf2daf90ed372
Eva Martin del Pico <eva.mdpico@gmail.com>
2025-02-28 16:52:11 +0100

    fix: repos functions not considering non repos

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -46,14 +46,15 @@ def is_github_repo(url):
     Checks if the url is a github repository.
     - url: url to check
     '''
-    if 'github.com' in url:
-        end =  url.split('github.com/')[1]
-        owner = end.split('/')[0]
-        repo = end.split('/')[1]
+    if 'github.com/' in url:
+        if len(url.split('github.com/'))>1:
+            end =  url.split('github.com/')[1]
+            if len(end.split('/'))>=2:
+                owner = end.split('/')[0]
+                repo = end.split('/')[1]
 
-        clean_repo = f"https://github.com/{owner}/{repo}"
-
-        return clean_repo
+                clean_repo = f"https://github.com/{owner}/{repo}"
+                return clean_repo
         
     else:
         return None
@@ -63,14 +64,15 @@ def is_gitlab_repo(url):
     Checks if the url is a gitlab repository.
     - url: url to check
     '''
-    if 'gitlab.com' in url:
-        end =  url.split('gitlab.com/')[1]
-        owner = end.split('/')[0]
-        repo = end.split('/')[1]
-        
-        clean_repo = f"https://gitlab.com/{owner}/{repo}"
-
-        return clean_repo
+    if 'gitlab.com/' in url:
+        if len(url.split('gitlab.com/'))>1:
+            end =  url.split('gitlab.com/')[1]
+            if len(end.split('/'))>=2:
+                owner = end.split('/')[0]
+                repo = end.split('/')[1]
+                
+                clean_repo = f"https://gitlab.com/{owner}/{repo}"
+                return clean_repo
         
     else:
         return None
@@ -81,12 +83,14 @@ def is_bitbucket_repo(url):
     Checks if the url is a bitbucket repository.
     - url: url to check
     '''
-    if 'bitbucket.org' in url:
-        end =  url.split('bitbucket.org/')[1]
-        owner = end.split('/')[0]
-        repo = end.split('/')[1]
+    if 'bitbucket.org/' in url:
+        if len(url.split('bitbucket.org/'))>1:
+            end =  url.split('bitbucket.org/')[1]
+            if len(end.split('/'))>=2:
+                owner = end.split('/')[0]
+                repo = end.split('/')[1]
         
-        clean_repo = f"https://bitbucket.org/{owner}/{repo}"
+                clean_repo = f"https://bitbucket.org/{owner}/{repo}"
         return clean_repo
         
     else:
