a70ae2311ae87f46bcccc36e703eb23a98338adb
Eva Martin del Pico <eva.mdpico@gmail.com>
2025-03-27 12:00:11 +0100

    sources as argument in transformation cli

--- src/adapters/cli/transformation/transformation.py
+++ src/adapters/cli/transformation/transformation.py
@@ -1,5 +1,18 @@
 """
 The command-line interface for the transformer
+possible sources : [
+    'bioconda', 
+    'bioconda_recipes', 
+    'github', 
+    'biotools', 
+    'bioconductor', 
+    'galaxy_metadata', 
+    'toolshed', 
+    'galaxy', 
+    'sourceforge', 
+    'opeb_metrics'
+    ]
+
 """
 import argparse
 import logging
@@ -22,24 +35,43 @@ def main():
         default=".env",
     )
 
+    parser.add_argument(
+        "--sources-to-transform", "-s",
+        help=("Sources to transform. The posiibliities are: bioconda, bioconda_recipes, github, biotools, bioconductor, galaxy_metadata, toolshed, galaxy, sourceforge and opeb_metrics. Default is all sources."),
+        type=list,
+        default=[
+            "bioconda", 
+            "bioconda_recipes", 
+            "github", 
+            "biotools", 
+            "bioconductor", 
+            "galaxy_metadata", 
+            "toolshed", 
+            "galaxy", 
+            "sourceforge", 
+            "opeb_metrics"
+        ],
+        dest="sources"
+    )
+
     args = parser.parse_args()
 
+
+    # Load the environment variables ------------------------------------------
+    logger.debug(f"Env file: {args.env_file}")
     load_dotenv(args.env_file)
 
+    # import here so the env variables are loaded before the initialization of the db client (which uses them to connect)
     from src.application.use_cases.transformation.main import transform_sources
-
-
-    logger.debug(f"Env file: {args.env_file}")
-
+    
+    # Transform the sources ---------------------------------------------------
+    logger.info(f"Sources to transform: {args.sources}")
     logger.info("Transforming raw data...")
 
-    # COULD DO: take sources from argparse
-    # sources = ['bioconda', 'bioconda_recipes', 'github', 'biotools', 'bioconductor', 'galaxy_metadata', 'toolshed', 'galaxy', 'sourceforge', 'opeb_metrics']
+    transform_sources(sources=args.sources)
 
-    sources = ['bioconda_recipes']
-
-    transform_sources(sources=sources)
-    logger.info("Transformation successful!")
+    # Finish ------------------------------------------------------------------
+    logger.info("Transformation finished!")
 
 if __name__ == "__main__":
     main()
